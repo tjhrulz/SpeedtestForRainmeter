72084a3e9254e9879b9764e324b9da9056368764
Trevor Hamilton <tjh1003@sru.edu>
2017-05-02 14:27:04 -0400

    Streamlined website support
    Since fast.com only supports download and beta.sppedtest.net only works
    50% of the time and loads really slow I have decided to support just
    google's since their seem to be the most accurate anyways.
    
    Google support works 100% of the time without issue and provides updates
    while it is running. The only issue is that all but the last update are
    off by a factor of 100. At first I though I was doing something wrong
    but it the webpage (I included an example) it clearly says 400 something
    instead of 4. I think it has something to do with my user agent, gonna
    try a few things before I juset shift the decimal over two places.

--- loadspeed.js
+++ loadspeed.js
@@ -1,123 +1,76 @@
 var page = require("webpage").create();
-system = require("system");
+var fs = require('fs');
+var outputCount = 0;
+address = "https://www.google.com/search?q=speedtest";
 
-if (system.args.length === 1) {
-    console.log("Usage: loadspeed.js fast|speedtest|google");
-    phantom.exit(1);
-} else {
-    var whichAddress = system.args[1];
+page.settings.userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36";
+page.open(address, function(status) {
+	if (status !== "success") {
+		console.log("Check internet connection");
+	}
+	else {
+		//console.log("Starting google search speedtest");
 
-	if(whichAddress.indexOf("fast") !== -1)
-	{
-		address = "https://fast.com/";		
+		runSpeedtest();
 	}
-	else if(whichAddress.indexOf("speedtest") !== -1)
-	{
-		address = "http://beta.speedtest.net/";		
-	}
-	else if(whichAddress.indexOf("google") !== -1)
-	{
-		address = "https://www.google.com/search?q=speedtest&oq=speedtest";
-		//address = "http://speedtest.googlefiber.net/";		
-	}
-	else
-	{
-		console.log("Address given no recognized, using fast.com");	
-		address = "https://fast.com/";	
-	}
-	console.log("Using address:" + address);	
-	
-	page.settings.userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36";
-	page.open(address, function (status) 
-	{
-		if (status !== "success") 
-		{
-			console.log("FAIL to load the address");
-		} 
-		else 
-		{
-			setTimeout(function(){
-				setTimeout(function(){
-					if(whichAddress.indexOf("fast") !== -1)
-					{
-						console.log("Starting fast.com speedtest");
-						setTimeout(function(){
-							var download = page.evaluate(function () {
-								return document.getElementById("speed-value").innerHTML;
-							});
-							console.log("P:" + "" + " D:" + download + " U:" + "");
-							
-							phantom.exit();
-						},10000);
-					}
-					else if(whichAddress.indexOf("speedtest") !== -1)
-					{
-						console.log("Starting beta.speedtest.net speedtest");
-						console.log("Found button:" + page.evaluate(function() {
-							return document.getElementsByClassName("start-text")[0].innerHTML;
-						}));
-						
-						page.evaluate(function(){
-							var a = document.getElementsByClassName("js-start-test start-button-measured")[0];
-							var e = document.createEvent('MouseEvents');
-							e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-							a.dispatchEvent(e);
-							waitforload = true;
-						});						
-						
-						setTimeout(function(){
-							var ping = page.evaluate(function () {
-								return document.getElementById("ping-value").innerHTML;
-							});
-							var download = page.evaluate(function () {
-								return document.getElementsByClassName("result-data-large number result-data-value download-speed")[0].innerHTML;
-							});
-							var upload = page.evaluate(function () {
-								return document.getElementsByClassName("result-data-large number result-data-value upload-speed")[0].innerHTML;
-							});
-							console.log("P:" + ping + " D:" + download + " U:" + upload);
-							
-							//console.log('Page body is ' + page.evaluate(function () {
-							//	return document.body.innerHTML;
-							//}));							
-							
-							console.log("Found button:" + page.evaluate(function() {
-								return document.getElementsByClassName("start-text")[0].innerHTML;
-							}));
-							phantom.exit();
-						},60000);
-					}
-					else if(whichAddress.indexOf("google") !== -1)
-					{
-						console.log("Starting google search speedtest");
-						
-						page.evaluate(function(){
-							var a = document.getElementById("lrfactory-internetspeed__test_button");
-							var e = document.createEvent('MouseEvents');
-							e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-							a.dispatchEvent(e);
-							waitforload = true;
-						});
-						
-						setTimeout(function(){
-							var download = page.evaluate(function () {
-								return document.getElementsByClassName("_ylk iI6lIEYZcPmM-kMKCIXyuNCs")[0].innerHTML;
-							});
-							var upload = page.evaluate(function () {
-								return document.getElementsByClassName("_ylk iI6lIEYZcPmM-Ehhee1Gox1M")[0].innerHTML;
-							});
-							console.log("P:" + "" + " D:" + download + " U:" + upload);
+});
 
-							phantom.exit();
-						},60000);
-					}
-					else
-					{
-						console.log("Page unknown");
-						phantom.exit();
-					}
-				},100);
-			},1);
-		}
+
+function runSpeedtest() {
+	//console.log("Running google search speedtest");
+	clickStart();
+	updater = setInterval(updateSpeedtestData, 150);
+}
+
+function clickStart() {
+	page.evaluate(function() {
+		var a = document.getElementById("lrfactory-internetspeed__test_button");
+		var e = document.createEvent('MouseEvents');
+		e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		a.dispatchEvent(e);
 	});
+}
+
+function updateSpeedtestData() {
+	var info = page.evaluate(function() {
+		return document.getElementsByClassName("lrfactory-internetspeed__status-indicator")[0].innerText.split("\n");
+	});
+
+  //console.log(info);
+
+	if (info.length > 3 && info[3] === "Testing download...") {
+		console.log("D: " + info[1] + " " + info[2]);
+	}
+	else if (info.length > 3 && info[3] === "Testing upload...") {
+		console.log("U: " + info[1] + " " + info[2]);
+    writeCurrPageToFile();
+		var possibleFinalData = page.evaluate(function() {
+			return document.getElementById("lrfactory-internetspeed__upload").innerText.split("\n")[0];
+		});
+		if (possibleFinalData !== " ") {
+			clearInterval(updater);
+			finalSpeedtestData();
+		}
+	}
+}
+
+function finalSpeedtestData() {
+  var ping = page.evaluate(function() {
+    return document.getElementById("lrfactory-internetspeed__latency").innerText.replace("Latency: ", "");
+  });
+	var download = page.evaluate(function() {
+		return document.getElementById("lrfactory-internetspeed__download").innerText.replace(/\n/gi, "").replace(" download", "");
+	});
+	var upload = page.evaluate(function() {
+		return document.getElementById("lrfactory-internetspeed__upload").innerText.replace(/\n/gi, "").replace(" upload", "");
+	});
+  
+	console.log("P:" + ping + " D:" + download + " U:" + upload);
+	phantom.exit();
+}
+
+function writeCurrPageToFile()
+{
+  fs.write(outputCount + "output.html", page.evaluate( function(){ return document.body.innerHTML; }), 'w');
+  outputCount++;
 }
