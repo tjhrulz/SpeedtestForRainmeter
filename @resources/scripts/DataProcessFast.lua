f7de65d305418157ea1b047e67cae90762fcad6b
khanhas <xuankhanh963@gmail.com>
2017-05-05 15:39:40 +0700

    Occasionally fail to display final result fixed

--- @resources/scripts/DataProcessFast.lua
+++ @resources/scripts/DataProcessFast.lua
@@ -9,27 +9,27 @@ percent = 0
 reading = false
 thereError = false
 curMeasure = "Welcome"
-color={"fast","google","speedof","bandwidthplace","xfinity","atandt","comcast","verizon"}
-color.fast	 		= {['sR'] = 229, ['sG'] = 009, ['sB'] = 020, ['lH1'] = 'cb2d3e', ['lH2'] = 'ef473a'}
-color.google  		= {['sR'] = 000, ['sG'] = 145, ['sB'] = 234, ['lH1'] = '36D1DC', ['lH2'] = '5B86E5'}
-color.speedof 		= {['sR'] = 033, ['sG'] = 060, ['sB'] = 046, ['lH1'] = '000000', ['lH2'] = '000000'}
-color.bandwidthplace= {['sR'] = 243, ['sG'] = 070, ['sB'] = 000, ['lH1'] = 'FF512F', ['lH2'] = 'F09819'}
-color.xfinity		= {['sR'] = 046, ['sG'] = 160, ['sB'] = 221, ['lH1'] = '000000', ['lH2'] = '000000'}
-color.atandt		= {['sR'] = 243, ['sG'] = 070, ['sB'] = 000, ['lH1'] = '000000', ['lH2'] = '000000'}
+color={"default","fast","google","speedof","bandwidthplace","xfinity","atandt","comcast","verizon"}
+color.default		= {['sR'] = 011, ['sG'] = 018, ['sB'] = 031}
+color.fast			= {['sR'] = 229, ['sG'] = 009, ['sB'] = 020, ['lH1'] = 'cb2d3e', ['lH2'] = 'ef473a'}
+color.google		= {['sR'] = 000, ['sG'] = 145, ['sB'] = 234, ['lH1'] = '36D1DC', ['lH2'] = '5B86E5'}
 color.verizon		= {['sR'] = 205, ['sG'] = 004, ['sB'] = 011, ['lH1'] = '8E0E00', ['lH2'] = '1F1C18'}
+color.bandwidthplace= {['sR'] = 243, ['sG'] = 070, ['sB'] = 000, ['lH1'] = 'FF512F', ['lH2'] = 'F09819'}
+--color.speedof		= {['sR'] = 033, ['sG'] = 060, ['sB'] = 046, ['lH1'] = '000000', ['lH2'] = '000000'}
+--color.xfinity		= {['sR'] = 046, ['sG'] = 160, ['sB'] = 221, ['lH1'] = '000000', ['lH2'] = '000000'}
+--color.atandt		= {['sR'] = 243, ['sG'] = 070, ['sB'] = 000, ['lH1'] = '000000', ['lH2'] = '000000'}
 
 function Initialize()
 	cursite = SKIN:GetVariable('defaultsite')
 	site = cursite
 	sbg('SetOption LoadingBar Grad \"180|'..color[site]["lH1"]..'b4;0|'..color[site]["lH2"]..'b4;1','UpdateMeter LoadingBar',
-		'SetOptionGroup Site FontColor ' ..color[site]["sR"]..','..color[site]["sG"]..','..color[site]["sB"]..',56',
+		'SetOptionGroup Site FontColor ' ..color["default"]["sR"]..','..color["default"]["sG"]..','..color["default"]["sB"]..',56',
 		'SetOption '..site..' FontColor '..color[site]["sR"]..','..color[site]["sG"]..','..color[site]["sB"],
 		'UpdateMeterGroup Site')
 end
 
 function Update()
 	measures = {}
-	details = {}
 	speeds = {}
 	units = {}
 
@@ -40,39 +40,43 @@ function Update()
 		file:close()
 		oSite = contents:match("Site:%s(%w+)")
 
-		for measure,detail in string.gmatch(contents,"\n([PUDF]):%s(.-)\n") do
+		for measure,speed,unit in string.gmatch(contents,"\n([PUD]):%s(.-)%s(%w+)\n") do
 			table.insert(measures,measure)
-			table.insert(details,detail)
-			for speed,unit in detail:gmatch("(.-)%s(%w+)") do
-				table.insert(speeds,speed)
-				table.insert(units,unit)
-			end
+			table.insert(speeds,speed)
+			table.insert(units,unit)
 		end
 
-		if measures[#measures] ~= nil then
-			if measures[#measures] ~= curMeasure then 
+		final,finalDetail = string.match(contents,"(F):%s(.*)\n") --Find final result
+
+		if measures[#measures] ~= nil then 
+			if measures[#measures] ~= curMeasure then  --Switch measurement trigger
 				oldMeasure = curMeasure
 				curMeasure = measures[#measures]
 				timing6 = 1
 			end
 		end
 
-		if measures[#measures] == "F" then
+		if final == "F" then
 			local split = 0
-			for finalN,finalV in string.gmatch(details[#details],"(.):%s(.-%s%w+)") do
+			--Display all available final result
+			for finalN,finalV in string.gmatch(finalDetail,"(.):%s(.-%s%w+)") do
 				if finalV == '-1 error' then
 					reading = false
 					thereError = true
 					timing = 1
 					return
 				end
+
 				sbg('SetOption '..finalN..' FontSize 14',
 					'SetOption '..finalN..'Icon FontSize 16',
 					'SetOptionGroup '..finalN..' FontColor \"255,255,255,(255*#*final*#)\"',
-					'SetOptionGroup '..finalN..' Y \"(60*#*final*#-34)\"',
-					'SetOption '..finalN..'Icon X '..(40+130*split),
-					'SetOption '..finalN..' X '..(65+130*split),
+					'SetOptionGroup '..finalN..' Y \"(60*#*final*#-31)\"',
+					'SetOption '..finalN..'Icon X 15R',
+					'SetOption '..finalN..' X 5R',
 					'SetOPtion '..finalN..' Text \"'..finalV..'\"')
+
+				if split == 0 then sbg('SetOption '..finalN..'Icon X 40') end
+
 				split = split + 1
 			end
 			uGY = SKIN:GetMeter('UGraph'):GetY()
@@ -127,25 +131,32 @@ function Update()
 		percentfin = percent + (1-percent)*outQuart(timing3,0,1,10) --Push percent bar to 100 percent
 		sbg('SetVariable percent '..percentfin,'UpdateMeter LoadingBar')
 	elseif timing3 == 10 then
+		sbg('SetVariable percent 1','UpdateMeter LoadingBar')
 		timing3 = 11
 		timing4 = 1
 	end
 
-	if timing4 > 0 and timing4 < 100 then
+	if timing4 > 0 and timing4 < 50 then
 		timing4 = timing4 + 1
-	elseif timing4 == 100 then
-		timing4 = 101
-		sbg('SetOption Stop Text ','UpdateMeter Stop') --Change to Restart symbol
+		sbg('SetOptionGroup M InlineSetting3 \"Shadow|0|0|'..(5*timing4/50)..'|000000\"',
+			'UpdateMeterGroup M')
+	elseif timing4 == 50 then
+		timing4 = 51
+		sbg('SetOption Stop Text ','UpdateMeter Stop','ShowMeter Share') --Change to Restart symbol
 	end
 
-	if timing5 > 0 and timing5 < 40 then --Change Sites color
+	if timing5 > 0 and timing5 < 20 then --Change Sites color
 		timing5 = timing5 + 1
-		siteAnimate=outQuart(timing5,0,1,40)
-		sbg('SetOptionGroup Site FontColor ' ..(color[cursite]["sR"]+(color[site]["sR"]-color[cursite]["sR"])*siteAnimate)..','..(color[cursite]["sG"]+(color[site]["sG"]-color[cursite]["sG"])*siteAnimate)..','..(color[cursite]["sB"]+(color[site]["sB"]-color[cursite]["sB"])*siteAnimate)..',56',
-			'SetOption '..site..' FontColor '..(color[cursite]["sR"]+(color[site]["sR"]-color[cursite]["sR"])*siteAnimate)..','..(color[cursite]["sG"]+(color[site]["sG"]-color[cursite]["sG"])*siteAnimate)..','..(color[cursite]["sB"]+(color[site]["sB"]-color[cursite]["sB"])*siteAnimate),
-			'UpdateMeterGroup Site')
-	elseif timing5 == 40 then
-		timing5 = 41
+		siteAnimate=outQuart(timing5,0,1,20)
+		gottagofastAnimate=outElastic(timing5,0,1,20,0.1)
+
+		sbg('SetOptionGroup Site FontColor ' ..color["default"]["sR"]..','..color["default"]["sG"]..','..color["default"]["sB"]..',56',
+			'SetOption '..site..' FontColor '..(color["default"]["sR"]+(color[site]["sR"]-color["default"]["sR"])*siteAnimate)..','..(color["default"]["sG"]+(color[site]["sG"]-color["default"]["sG"])*siteAnimate)..','..(color["default"]["sB"]+(color[site]["sB"]-color["default"]["sB"])*siteAnimate),
+			'UpdateMeterGroup Site',
+			'SetOption Base Shape \"Rectangle (['..cursite..':X]+(['..site..':X]-['..cursite..':X])*'..gottagofastAnimate..'-10),27,(['..cursite..':W]+(['..site..':W]-['..cursite..':W])*'..gottagofastAnimate..'+20),60,3 | StrokeWidth 0 | Fill LinearGradient Shadow\"',
+			'UpdateMeter Base')
+	elseif timing5 == 20 then
+		timing5 = 21
 	end
 
 	if timing6 > 0 and timing6 < 20 then --Change measure
@@ -153,9 +164,9 @@ function Update()
 		switchAnimate=outQuart(timing6,0,1,20)
 
 		if curMeasure ~= 'F' then
-			sbg('SetOptionGroup '..oldMeasure..' Y '..(26 - 60 * switchAnimate),
+			sbg('SetOptionGroup '..oldMeasure..' Y '..(30 - 60 * switchAnimate),
 				'SetOptionGroup '..oldMeasure..' FontColor 255,255,255,'..(255-255*switchAnimate),
-				'SetOptionGroup '..curMeasure..' Y '..(-34 + 60 * switchAnimate),
+				'SetOptionGroup '..curMeasure..' Y '..(-30 + 60 * switchAnimate),
 				'SetOptionGroup '..curMeasure..' FontColor 255,255,255,'..255*switchAnimate,
 				'SetOption '..oldMeasure..'Graph Y '..(45 - 50 * switchAnimate),
 				'SetOption '..curMeasure..'Graph Y '..(-5 + 50 * switchAnimate),
@@ -166,17 +177,17 @@ function Update()
 		timing6 = 21
 	end
 
-	if timing7 > 0 and timing7 < 20 then --Final result out
+	if timing7 > 0 and timing7 < 40 then --Final result out
 		timing7 = timing7 + 1
-		slideoutAnimate = outQuart(timing7,0,1,20)
+		slideoutAnimate = outQuart(timing7,0,1,40)
 		sbg('SetVariable final '..slideoutAnimate,'UpdateMeterGroup F',
 			'SetOption UGraph Y '..uGY-50*slideoutAnimate,
 			'SetOption DGraph Y '..dGY-50*slideoutAnimate,
 			'SetOption PGraph Y '..pGY-50*slideoutAnimate,
 			'UpdateMeterGroup G')
-	elseif timing7 == 20 then
+	elseif timing7 == 40 then
 		sbg('SetOptionGroup M ClipStringW 100','SetOptionGroup M ClipString 2','SetVariable final 1','UpdateMeterGroup F')
-		timing7 = 21
+		timing7 = 41
 	end
 end
 
@@ -186,11 +197,6 @@ function sbg(...) --concentrate bangs
 		bangs = bangs .. '[!' .. tostring(v) .. ']'
 	end
 	SKIN:Bang(bangs)
-end
-
-function outQuart(t, b, c, d)
-  t = t / d - 1
-  return -c * (math.pow(t, 4) - 1) + b
 end
 
 function graph(s)
@@ -246,4 +252,30 @@ function changesite(s)
 	site = s
 	sbg('SetOption LoadingBar Grad \"180|'..color[site]["lH1"]..'b4;0|'..color[site]["lH2"]..'b4;1','UpdateMeter LoadingBar')
 	timing5 = 1
+end
+
+function outElastic(t, b, c, d, a, p)
+  if t == 0 then return b end
+
+  t = t / d
+
+  if t == 1 then return b + c end
+
+  if not p then p = d * 0.8 end
+
+  local s
+
+  if not a or a < math.abs(c) then
+    a = c
+    s = p / 4
+  else
+    s = p / (2 * math.pi) * math.asin(c/a)
+  end
+
+  return a * math.pow(2, -10 * t) * math.sin((t * d - s) * (2 * math.pi) / p) + c + b
+end
+
+function outQuart(t, b, c, d)
+  t = t / d - 1
+  return -c * (math.pow(t, 4) - 1) + b
 end
